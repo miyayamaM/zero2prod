a31c789c7abeda55338152914c7172d41f909228
miyayamaM <pendulum.ngt.4010@gmail.com>
2022-10-15 20:52:25 +0900

    modify js

--- dangerfile.js
+++ dangerfile.js
@@ -3,14 +3,21 @@ import { danger } from 'danger';
 const incomingBranchName = danger.github.pr.head.ref
 const baseBranchName = danger.github.pr.base.ref
 
-// mainブランチにはhotfix/*, release/*のみマージ可能
-if (baseBranchName === 'main') {
-  if (!incomingBranchName.startsWith('hotfix/') || !incomingBranchName.startsWith('release/')) {
-    fail(baseBranchName + ' branch can only be merged with hotfix/release branch')
+const allowedMergingBranchList = {
+  'main': ['hotfix', 'release'],
+  'develop': ['feature']
+}
+
+const allowedMergingBranch = allowedMergingBranchList[baseBranchName]
+
+let canMerge = false;
+
+for (var allowedPrefix in allowedMergingBranch) {
+  if (incomingBranchName.startsWith(allowedPrefix)) {
+    canMerge = true
   }
 }
 
-// developブランチにはfeature/*のみマージ可能
-if (baseBranchName === 'develop' && !incomingBranchName.startsWith('feature/')) {
-  fail(baseBranchName + ' branch can only be merged with feature branch')
+if (!canMerge) {
+  fail('Merging ' + baseBranchName + ' branch with ' + incomingBranchName + 'branch is not allowed.')
 }
