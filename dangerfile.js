9a60b9f2700b6883ad45bd3e8a1cb8bc3ae5b9bd
miyayamaM <pendulum.ngt.4010@gmail.com>
2022-10-15 20:39:58 +0900

    feat: limit merge branch prefix

--- dangerfile.js
+++ dangerfile.js
@@ -3,4 +3,16 @@ import { danger } from 'danger';
 const incomingBranchName = danger.github.pr.head.ref
 const baseBranchName = danger.github.pr.base.ref
 
-fail('You will merge' + baseBranchName + 'from' + incomingBranchName)
+// mainブランチにはhotfix/*, release/*のみマージ可能
+if (baseBranchName === 'main' && !incomingBranchName.startsWith('hotfix/')) {
+  fail(baseBranchName + ' branch can only be merged with' + 'feature branch')
+}
+
+if (baseBranchName === 'main' && !incomingBranchName.startsWith('release/')) {
+  fail(baseBranchName + ' branch can only be merged with' + 'feature branch')
+}
+
+// developブランチにはfeature/*のみマージ可能
+if (baseBranchName === 'develop' && !incomingBranchName.startsWith('feature/')) {
+  fail(baseBranchName + ' branch can only be merged with' + 'feature branch')
+}
