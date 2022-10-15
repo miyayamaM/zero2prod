60f291bdfd74dd0e7b125f749176d7ca7e353a74
miyayamaM <pendulum.ngt.4010@gmail.com>
2022-10-15 18:52:55 +0900

    feat: add danger

---
+++ danger.js
@@ -0,0 +1,8 @@
+const hasChangelog = danger.git.modified_files.includes('CHANGELOG.md')
+const isTrivial = (danger.github.pr.body + danger.github.pr.title).includes(
+  '#trivial'
+)
+
+if (!hasChangelog && !isTrivial) {
+  warn('Please add a changelog entry for your changes.')
+}
