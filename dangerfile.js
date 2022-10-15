0a60ccb36c5e876207a6f581a98b65b56e040558
miyayamaM <pendulum.ngt.4010@gmail.com>
2022-10-15 20:41:42 +0900

    feat: fix

--- dangerfile.js
+++ dangerfile.js
@@ -4,12 +4,10 @@ const incomingBranchName = danger.github.pr.head.ref
 const baseBranchName = danger.github.pr.base.ref
 
 // mainブランチにはhotfix/*, release/*のみマージ可能
-if (baseBranchName === 'main' && !incomingBranchName.startsWith('hotfix/')) {
-  fail(baseBranchName + ' branch can only be merged with' + 'feature branch')
-}
-
-if (baseBranchName === 'main' && !incomingBranchName.startsWith('release/')) {
-  fail(baseBranchName + ' branch can only be merged with' + 'feature branch')
+if (baseBranchName === 'main') {
+  if (!incomingBranchName.startsWith('hotfix/') || !incomingBranchName.startsWith('release/')) {
+    fail(baseBranchName + ' branch can only be merged with' + 'hotfix/release branch')
+  }
 }
 
 // developブランチにはfeature/*のみマージ可能
