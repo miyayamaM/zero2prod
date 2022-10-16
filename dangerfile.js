ee80a33678760a1512c2d20cd3deaa440db28b20
miyayamaM <pendulum.ngt.4010@gmail.com>
2022-10-17 08:47:42 +0900

    test

--- dangerfile.js
+++ dangerfile.js
@@ -1,21 +1,30 @@
 // import danger from 'danger';
 
-const incomingBranchName = danger.github.pr.head.ref
+// const incomingBranchName = danger.github.pr.head.ref
+// const baseBranchName = danger.github.pr.base.ref
+
+// const allowedMergingBranchList = {
+//   'main': ['hotfix', 'release'],
+//   'develop': ['feature']
+// }
+
+// let canMerge = false;
+
+// for (var allowedPrefix in allowedMergingBranchList[baseBranchName]) {
+//   if (incomingBranchName.startsWith(allowedPrefix)) {
+//     canMerge = true
+//   }
+// }
+
+// if (!canMerge) {
+//   fail('Merging ' + baseBranchName + ' branch with ' + incomingBranchName + ' branch is not allowed.')
+// }
+
+import { danger } from 'danger';
+
+const headBranchName = danger.github.pr.head.ref
 const baseBranchName = danger.github.pr.base.ref
 
-const allowedMergingBranchList = {
-  'main': ['hotfix', 'release'],
-  'develop': ['feature']
-}
-
-let canMerge = false;
-
-for (var allowedPrefix in allowedMergingBranchList[baseBranchName]) {
-  if (incomingBranchName.startsWith(allowedPrefix)) {
-    canMerge = true
-  }
-}
-
-if (!canMerge) {
-  fail('Merging ' + baseBranchName + ' branch with ' + incomingBranchName + ' branch is not allowed.')
+if (baseBranchName === 'main' && !headBranchName.startsWith('release/')) {
+  fail(baseBranchName + ' branch can only be merged with ' + headBranchName + ' branch.')
 }
