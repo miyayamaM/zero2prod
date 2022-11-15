44e177c497f777298f0d0c32095f8fcdb9f28297
miyayamaM <pendulum.ngt.4010@gmail.com>
2022-11-15 22:20:31 +0900

    feat: docker container

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,13 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let connection = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let connection =
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
+            .expect("Failed to connect to Postgres.");
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener, connection)?.await
 }
